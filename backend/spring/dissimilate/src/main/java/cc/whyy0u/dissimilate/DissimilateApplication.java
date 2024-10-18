c503fd3482a841a794749cbe68fd70e097bea05a
WhyY0u <aimsi13579@gmail.com>
2024-10-18 17:11:08 -0400

    First

---
+++ backend/spring/dissimilate/src/main/java/cc/whyy0u/dissimilate/DissimilateApplication.java
@@ -0,0 +1,13 @@
+package cc.whyy0u.dissimilate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DissimilateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DissimilateApplication.class, args);
+	}
+
+}
---
+++ backend/spring/dissimilate/src/test/java/cc/whyy0u/dissimilate/DissimilateApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.whyy0u.dissimilate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DissimilateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
