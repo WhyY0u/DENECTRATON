953ac1450cdf350d6dd902211bb0415543b6c485
Akiro <zantrebatyrbek@gmail.com>
2024-10-18 17:21:56 +0500

    fixed

---
+++ frontend/src/components/Login/LoginForm/LoginForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './styles/LoginForm.module.css'
+
+function LoginForm() {
+  return (
+    <div className={styles.login__form}>
+        <div className={`${styles.login__form__container} _container`}>
+            
+        </div>
+    </div>
+  )
+}
+
+export default LoginForm
